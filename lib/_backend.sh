aac0ca804bef129edc4012e5cb7b85c6d68733d7
Rafa Dev <30558052+rafaelmatostj@users.noreply.github.com>
2025-11-03 14:26:29 -0300

    Refactor backend setup and Docker configurations
    Refactor backend setup script to create Docker containers for PostgreSQL and Redis, and update Nginx configuration for backend service.

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -1,3 +1,339 @@
+#!/bin/bash
+# 
+# functions for setting up app backend
+
+#######################################
+# creates docker db
+# Arguments:
+#   None
+#######################################
+backend_db_create() {
+  print_banner
+  printf "${WHITE} 💻 Criando banco de dados...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  usermod -aG docker deploy
+  mkdir -p /data
+  chown -R 999:999 /data
+  docker run --name postgresql \
+                -e POSTGRES_USER=izing \
+                -e POSTGRES_PASSWORD=${pg_pass} \
+				-e TZ="America/Sao_Paulo" \
+                -p 5432:5432 \
+                --restart=always \
+                -v /data:/var/lib/postgresql/data \
+                -d postgres
+
+  docker run --name redis-izing \
+                -e TZ="America/Sao_Paulo" \
+                -p 6379:6379 \
+                --restart=always \
+                -d redis:latest redis-server \
+                --appendonly yes \
+                --requirepass "${redis_pass}"
+
+  docker run -d --name portainer \
+                -p 9000:9000 -p 9443:9443 \
+                --restart=always \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -v portainer_data:/data portainer/portainer-ce
+EOF
+
+  sleep 2
+}
+
+#######################################
+# install_chrome
+# Arguments:
+#   None
+#######################################
+backend_chrome_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalar Google Chrome...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+wget --inet4-only -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmour -o /usr/share/keyrings/chrome-keyring.gpg 
+sudo sh -c 'echo "deb [arch=amd64,arm64,ppc64el signed-by=/usr/share/keyrings/chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+sudo apt update 
+sudo apt install -y google-chrome-stable 
+EOF
+
+  sleep 2
+}
+
+#######################################
+# sets environment variable for backend.
+# Arguments:
+#   None
+#######################################
+backend_set_env() {
+  print_banner
+  printf "${WHITE} 💻 Configurando variáveis de ambiente (backend)...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  # ensure idempotency
+  backend_url=$(echo "${backend_url/https:\/\/}")
+  backend_url=${backend_url%%/*}
+  backend_url=https://$backend_url
+
+  # ensure idempotency
+  frontend_url=$(echo "${frontend_url/https:\/\/}")
+  frontend_url=${frontend_url%%/*}
+  frontend_url=https://$frontend_url
+  
+  jwt_secret=$(openssl rand -base64 32)
+  jwt_refresh_secret=$(openssl rand -base64 32)
+
+sudo su - deploy << EOF
+  cat <<[-]EOF > /home/deploy/${nome_instancia}/backend/.env
+NODE_ENV=dev
+BACKEND_URL=${backend_url}
+FRONTEND_URL=${frontend_url}
+
+PROXY_PORT=443
+PORT=${backend_porta}
+
+# conexão com o banco de dados
+DB_DIALECT=postgres
+DB_PORT=${porta_postgre_intancia}
+DB_TIMEZONE=-03:00
+POSTGRES_HOST=localhost
+POSTGRES_USER=izing
+POSTGRES_PASSWORD=${pg_pass}
+POSTGRES_DB=postgres
+
+# Chaves para criptografia do token jwt
+JWT_SECRET=${jwt_secret}
+JWT_REFRESH_SECRET=${jwt_refresh_secret}
+
+# Dados de conexão com o REDIS
+IO_REDIS_SERVER=localhost
+IO_REDIS_PASSWORD=${redis_pass}
+IO_REDIS_PORT=6379
+IO_REDIS_DB_SESSION=2
+
+#CHROME_BIN=/usr/bin/google-chrome
+CHROME_BIN=/usr/bin/google-chrome-stable
+
+# tempo para randomização da mensagem de horário de funcionamento
+MIN_SLEEP_BUSINESS_HOURS=10000
+MAX_SLEEP_BUSINESS_HOURS=20000
+
+# tempo para randomização das mensagens do bot
+MIN_SLEEP_AUTO_REPLY=4000
+MAX_SLEEP_AUTO_REPLY=6000
+
+# tempo para randomização das mensagens gerais
+MIN_SLEEP_INTERVAL=2000
+MAX_SLEEP_INTERVAL=5000
+
+# dados do RabbitMQ / Para não utilizar, basta comentar a var AMQP_URL
+RABBITMQ_DEFAULT_USER=admin
+RABBITMQ_DEFAULT_PASS=123456
+# AMQP_URL='amqp://admin:123456@host.docker.internal:5672?connection_attempts=5&retry_delay=5'
+
+# api oficial (integração em desenvolvimento)
+API_URL_360=https://waba-sandbox.360dialog.io
+
+# usado para mosrar opções não disponíveis normalmente.
+ADMIN_DOMAIN=izing.io
+
+# Dados para utilização do canal do facebook
+FACEBOOK_APP_ID=3237415623048660
+FACEBOOK_APP_SECRET_KEY=3266214132b8c98ac59f3e957a5efeaaa13500
+
+# Forçar utilizar versão definida via cache (https://wppconnect.io/pt-BR/whatsapp-versions/)
+#WEB_VERSION=2.2409.2
+
+# Customizar opções do pool de conexões DB
+#POSTGRES_POOL_MAX=100
+#POSTGRES_POOL_MIN=10
+#POSTGRES_POOL_ACQUIRE=30000
+#POSTGRES_POOL_IDLE=10000
+
+# Limitar Uso do Izing Usuario e Conexões
+USER_LIMIT=99
+CONNECTIONS_LIMIT=99
+[-]EOF
+EOF
+
+  sleep 2
+}
+
+
+#######################################
+# installs node.js dependencies
+# Arguments:
+#   None
+#######################################
+backend_node_dependencies() {
+  print_banner
+  printf "${WHITE} 💻 Instalando dependências do backend...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  npm install --force
+EOF
+
+  sleep 2
+}
+
+#######################################
+# compiles backend code
+# Arguments:
+#   None
+#######################################
+backend_node_build() {
+  print_banner
+  printf "${WHITE} 💻 Compilando o código do backend...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  npm run build
+EOF
+
+  sleep 2
+}
+
+#######################################
+# updates whatsapp.js
+# Arguments:
+#   None
+#######################################
+whatsappweb_update() {
+  print_banner
+  printf "${WHITE} 💻 Atualizando o whatsapp.js...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  pm2 stop all
+  rm .wwebjs_auth -Rf
+  rm .wwebjs_cache -Rf
+  rm package-lock.json
+  npm r whatsapp-web.js
+  npm install github:pedroslopez/whatsapp-web.js
+  pm2 restart all
+EOF
+
+  sleep 2
+}
+
+restart_pm2() {
+  print_banner
+  printf "${WHITE} 💻 Reiniciando PM2...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  pm2 restart all
+EOF
+
+  sleep 2
+}
+
+#######################################
+# updates izing
+# Arguments:
+#   None
+#######################################
+git_update() {
+  print_banner
+  printf "${WHITE} 💻 Atualizando o izing do git...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}
+  pm2 stop all
+  git stash clear
+  git stash
+  git pull
+EOF
+
+  sleep 2
+}
+
+#######################################
+# runs db migrate
+# Arguments:
+#   None
+#######################################
+backend_db_migrate() {
+  print_banner
+  printf "${WHITE} 💻 Executando db:migrate...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  npx sequelize db:migrate
+EOF
+
+  sleep 2
+}
+
+#######################################
+# runs db seed
+# Arguments:
+#   None
+#######################################
+backend_db_seed() {
+  print_banner
+  printf "${WHITE} 💻 Executando db:seed...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  npx sequelize db:seed:all
+EOF
+
+  sleep 2
+}
+
+#######################################
+# starts backend using pm2 in 
+# production mode.
+# Arguments:
+#   None
+#######################################
+backend_start_pm2() {
+  print_banner
+  printf "${WHITE} 💻 Iniciando pm2 (backend)...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  cd /home/deploy/${nome_instancia}/backend
+  pm2 start dist/server.js --name ${nome_instancia}-backend
+  pm2 save
+EOF
+
+  sleep 2
+}
+
 #######################################
 # updates frontend code
 # Arguments:
@@ -14,64 +350,25 @@ backend_nginx_setup() {
 
 sudo su - root << EOF
 
-cat > /etc/nginx/sites-available/${nome_instancia}-backend << END
+cat > /etc/nginx/sites-available/${nome_instancia}-backend << 'END'
 server {
   server_name $backend_hostname;
 
   location / {
     proxy_pass http://127.0.0.1:${backend_porta};
     proxy_http_version 1.1;
-
     proxy_set_header Upgrade \$http_upgrade;
-    proxy_set_header Connection \$connection_upgrade;
-
+    proxy_set_header Connection 'upgrade';
     proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-Proto \$scheme;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_cache_bypass \$http_upgrade;
-
-    # timeout extendido para conexões websocket
-    proxy_read_timeout 3600s;
-    proxy_send_timeout 3600s;
-  }
-
-  # Redirect HTTP -> HTTPS
-  listen 80;
-  listen [::]:80;
-  return 301 https://\$host\$request_uri;
-}
-
-server {
-  listen 443 ssl http2;
-  listen [::]:443 ssl http2;
-  server_name $backend_hostname;
-
-  ssl_certificate /etc/letsencrypt/live/$backend_hostname/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/$backend_hostname/privkey.pem;
-
-  location / {
-    proxy_pass http://127.0.0.1:${backend_porta};
-    proxy_http_version 1.1;
-
-    proxy_set_header Upgrade \$http_upgrade;
-    proxy_set_header Connection \$connection_upgrade;
-
-    proxy_set_header Host \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_cache_bypass \$http_upgrade;
-
-    proxy_read_timeout 3600s;
-    proxy_send_timeout 3600s;
   }
 }
 END
 
-ln -sf /etc/nginx/sites-available/${nome_instancia}-backend /etc/nginx/sites-enabled/${nome_instancia}-backend
-nginx -t && systemctl restart nginx
-
+ln -s /etc/nginx/sites-available/${nome_instancia}-backend /etc/nginx/sites-enabled
 EOF
 
   sleep 2
